efb618f179669c78a80e160bfb4590ebe0e2203c
karolg02 <karolglanowski02@gmail.com>
2025-01-06 20:08:45 +0100

    +working

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,4 +1,1 @@
 export const API_URL = import.meta.env.VITE_API_BASE_URL;
-export const AZURE_SAN = import.meta.env.AZURE_SAN;
-export const AZURE_SST = import.meta.env.AZURE_SST;
-export const AZURE_CN = import.meta.env.AZURE_CN;

99905fdf70517461ed4dff3f8e163b63d95d99fd
gabrysjj <gjedrzej@student.agh.edu.pl>
2025-01-15 09:54:06 +0100

    +dodanie testów logowani, nieudanego dodania oferty i nieudanego dodania komentarz

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,52 +1,19 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
 declare namespace Cypress {
     interface Chainable {
         login(email: string, password: string): Chainable<void>;
+        uploadFile(selector: string, fileName: string): Chainable<void>;
     }
 }
 
 Cypress.Commands.add('login', (email: string, password: string) => {
     cy.visit('http://localhost:5173/login');
-    cy.get('input[name="email"]').type(email);
-    cy.get('input[name="password"]').type(password);
+    cy.get('input[placeholder="Enter your email"]').type(email);
+    cy.get('input[placeholder="Enter your password"]').type(password);
     cy.get('button[type="submit"]').click();
+});
 
-    // Zweryfikuj pomyślne logowanie
-    cy.url().should('include', '/dashboard');
+Cypress.Commands.add('uploadFile', (selector: string, fileName: string) => {
+    cy.get(selector).attachFile(fileName);
 });
